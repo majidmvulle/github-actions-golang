8bc840e4192cfd2b70221a0391fb36cde28ced98
Majid Mvulle <mamv@nodesagency.com>
2022-05-02 22:41:41 +0400

    update lambda handler function as per AWS docs

--- main.go
+++ main.go
@@ -8,9 +8,15 @@ import (
 	"github.com/majidmvulle/github-actions/app"
 )
 
-func Handler(anApp app.App) {
+func Handler(anApp app.App) error {
+	if anApp.A == 0 || anApp.B == 0 {
+		return fmt.Errorf("both A and B should be higher than 0")
+	}
+
 	fmt.Printf("Sum of %d and %d is: %d\n", anApp.A, anApp.B, app.AddTwoNumbers(anApp))
 	fmt.Printf("Difference of %d and %d is: %d\n", anApp.A, anApp.B, app.SubtractTwoNumbers(anApp))
+
+	return nil
 }
 
 func main() {
@@ -20,9 +26,13 @@ func main() {
 	if lambdaTaskRoot := os.Getenv("LAMBDA_TASK_ROOT"); lambdaTaskRoot != "" {
 		lambda.Start(Handler)
 	} else {
-		Handler(app.App{
+		err := Handler(app.App{
 			A: appA,
 			B: appB,
 		})
+
+		if err != nil {
+			panic(err)
+		}
 	}
 }
