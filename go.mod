7887ad16dfd1a2284b99f1824453e95fae9b5e8a
Majid Mvulle <mamv@nodesagency.com>
2022-05-02 22:07:05 +0400

    Include lambda handler

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/majidmvulle/github-actions
 
 go 1.18
+
+require github.com/aws/aws-lambda-go v1.31.1 // indirect
--- main.go
+++ main.go
@@ -2,14 +2,22 @@ package main
 
 import (
 	"fmt"
+	"github.com/aws/aws-lambda-go/lambda"
+	"os"
 
 	"github.com/majidmvulle/github-actions/app"
 )
 
-func main() {
-	a := 10
-	b := 5
-
+func Handler(a int, b int) {
 	fmt.Printf("Sum of %d and %d is: %d\n", a, b, app.AddTwoNumbers(a, b))
 	fmt.Printf("Difference of %d and %d is: %d\n", a, b, app.SubtractTwoNumbers(a, b))
+}
+
+func main() {
+
+	if lambdaTaskRoot := os.Getenv("LAMBDA_TASK_ROOT"); lambdaTaskRoot != "" {
+		lambda.Start(Handler)
+	} else {
+		Handler(48, 21)
+	}
 }
